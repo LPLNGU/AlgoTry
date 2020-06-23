7340e4ba8c1e7f1ec2ce69940cfac4904f261cbe
LPLNGU <1075542875@qq.com>
2020-06-23 14:57:56 +0800

    多线程聊天室（Socket）

---
+++ MultiThread/src/com/lpl/clientandserver/Client.java
@@ -0,0 +1,76 @@
+package com.lpl.clientandserver;
+
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    private Socket socket;
+
+    public Client(){
+        try {
+            socket = new Socket("localhost", 8088);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ServerHandler implements Runnable{
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            try{
+                InputStream is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+                while(true){
+                    System.out.println(br.readLine());
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void start(){
+        try{
+            ServerHandler handler = new ServerHandler();
+            Thread t = new Thread(handler);
+            t.setDaemon(true);
+            t.start();
+            OutputStream out = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(out, "UTF-8");
+            PrintWriter pw = new PrintWriter(osw, true);
+            //创建Scanner读取用户输入内容
+            Scanner scanner = new Scanner(System.in);
+            while(true){
+                pw.println(scanner.nextLine());
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            if(socket != null){
+                try{
+                    socket.close();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Client client = new Client();
+        client.start();
+    }
+
+}
