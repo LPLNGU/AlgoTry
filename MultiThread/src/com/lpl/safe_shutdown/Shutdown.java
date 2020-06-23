897b40c544149d03be6d99dbe454c160eb363ef1
LPLNGU <1075542875@qq.com>
2020-06-23 19:04:43 +0800

    线程学习

---
+++ MultiThread/src/com/lpl/safe_shutdown/Shutdown.java
@@ -0,0 +1,38 @@
+package com.lpl.safe_shutdown;
+
+import java.util.concurrent.TimeUnit;
+
+public class Shutdown {
+    public static void main(String[] args) throws InterruptedException {
+        Runner one = new Runner();
+        Thread countThread = new Thread(one, "CountThread");
+        countThread.start();
+        TimeUnit.SECONDS.sleep(1);
+        countThread.interrupt();
+        Runner two = new Runner();
+        countThread = new Thread(two, "CountThread");
+        countThread.start();
+        TimeUnit.SECONDS.sleep(1);
+        two.cancel();
+    }
+
+
+    private static class Runner implements Runnable {
+        private long i;
+        private volatile boolean on = true;
+
+        @Override
+        public void run() {
+            //当on并且没有被打断的情况下
+            while (on && !Thread.currentThread().isInterrupted()) {
+                i++;
+            }
+            System.out.println("Count i = " + i);
+        }
+
+        public void cancel() {
+            on = false;
+        }
+    }
+
+}
