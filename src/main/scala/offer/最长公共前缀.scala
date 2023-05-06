1b28ce0b9ce1969357a9512d1d2a9b569bdeae11
李沛隆 <21081020@suning.com>
2023-05-06 17:52:47 +0800

    syn

---
+++ src/main/scala/offer/最长公共前缀.scala
@@ -0,0 +1,15 @@
+package offer
+
+object 最长公共前缀 {
+  def longestCommonPrefix(strs: Array[String]): String = {
+    var index = 0
+    var res = ""
+    for (elem <- strs) {
+
+    }
+  }
+
+  def getLongestCommonPrefix(current: String, res: String): String = {
+
+  }
+}
